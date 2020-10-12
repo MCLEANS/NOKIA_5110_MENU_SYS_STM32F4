b0aad3afb242b4d686eb9c9980eab7b63558f972
MCLEANS <jackagwaya@gmail.com>
2020-10-12 09:47:57 +0300

    Initialized LCD'

--- main.cpp
+++ main.cpp
@@ -1,11 +1,38 @@
 #include "stm32f4xx.h"
 #include "clockconfig.h"
+#include "NOKIA_5110.h"
+
+#define RST_PORT GPIOD
+#define RST_PIN 0
+#define CS_PORT GPIOD
+#define CS_PIN 1
+#define DC_PORT GPIOD
+#define DC_PIN 2
 
 custom_libraries::clock_config system_clock;
+custom_libraries::NOKIA_5110 NOKIA(SPI1,
+                                    GPIOA,
+                                    5,
+                                    7,
+                                    6,
+                                    64,
+                                    false,
+                                    false,
+                                    false,
+                                    CS_PORT,
+                                    CS_PIN,
+                                    RST_PORT,
+                                    RST_PIN,
+                                    DC_PORT,
+                                    DC_PIN);
 
 int main(void) {
   
   system_clock.initialize();
+  NOKIA.inverted_mode();
+
+  NOKIA.print("MENU",30,0);
+
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
   
   GPIOA->MODER |= GPIO_MODER_MODER7_0;
